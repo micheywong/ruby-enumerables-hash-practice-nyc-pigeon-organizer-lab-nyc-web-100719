eff7acd7c6f7aedfbeb8c1a2125a5447d8a8a46a
Michelle Wong <michellekwong2@gmail.com>
2019-10-02 15:33:35 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,9 +2,10 @@ require "pry"
 def nyc_pigeon_organizer(data)
   names = { }
   
-  data.each do |key, value| 
-    puts "#{data[key]}"
+  data.each do |key, value_names| 
+      value_names.each do |color, names|
     binding.pry
+    end
   end
   names
 end
